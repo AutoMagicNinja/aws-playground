af77227c79774b41377a84300d214143377047dc
AutoMagicNinja <AutoMagicNinja@users.noreply.github.com>
2024-04-16 06:17:33 +0000

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A simple development environment for AWS CDK with TypeScript";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
+    mkShell {
+      buildInputs = [
+        nodejs
+        nodePackages.aws-cdk
+      ];
+      shellHook = ''
+        export NODE_PATH="${nodejs}/lib/node_modules:${nodePackages.aws-cdk}/lib/node_modules:$NODE_PATH"
+      '';
+    };
+
+  };
+}
+
